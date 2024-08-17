83b1f8f6c95563f0a6fb776ba58d883734701e94
Joshua Solidum <78305859+vmmandalorian@users.noreply.github.com>
2024-08-17 14:01:21 +0800

    Update Python_Basic_Training_Project.py

--- Python_Basic_Training_Project.py
+++ Python_Basic_Training_Project.py
@@ -1,4 +1,4 @@
-##Error checking with code making code hbprint("Number of letters in your name: " + len(input("Enter your name")))
+##Error checking with code , Make  the code work code print("Number of letters in your name: " + len(input("Enter your name")))
 Name1 = "Number of letters in your name: "
 Lenght_sum = str(len(input("Enter your name \n ")))
 print(Name1 + "" + Lenght_sum)
