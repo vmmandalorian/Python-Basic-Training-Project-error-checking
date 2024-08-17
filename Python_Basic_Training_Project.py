f76412942d68feb031f3cc5849ca0b0200ba3a97
vmmandalorian <josh.rema12@gmail.com>
2024-08-17 13:57:34 +0800

    Add project files.

---
+++ Python_Basic_Training_Project.py
@@ -0,0 +1,4 @@
+##Error checking with code making code hbprint("Number of letters in your name: " + len(input("Enter your name")))
+Name1 = "Number of letters in your name: "
+Lenght_sum = str(len(input("Enter your name \n ")))
+print(Name1 + "" + Lenght_sum)
